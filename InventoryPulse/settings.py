39995078cf3f92f4f6d8a2035885b199ad09daf6
5yn74x <41291962+plumped@users.noreply.github.com>
2025-03-23 02:43:26 +0100

    - documents (draft)

--- InventoryPulse/settings.py
+++ InventoryPulse/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -182,4 +182,5 @@ SELECT2_CACHE_BACKEND = "default"
 # Tesseract configuration
 TESSERACT_CMD = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Windows path
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- InventoryPulse/urls.py
+++ InventoryPulse/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from core import views as core_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # === Admin ===
@@ -130,3 +132,6 @@ urlpatterns = [
     # === Select2 ===
     path('select2/', include('django_select2.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
